d240036807865c69c5e4ea876056731fd7b5167d
shuvro168 <shuvroteam@gmail.com>
2026-07-13 14:30:35 +0600

    Update Supabase anon key in config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.APP_CONFIG={SUPABASE_URL:"https://asojxrnwbfccozjtndwa.supabase.co",SUPABASE_ANON_KEY:"PASTE_YOUR_PUBLISHABLE_KEY_HERE"};
+window.APP_CONFIG={SUPABASE_URL:"https://asojxrnwbfccozjtndwa.supabase.co",SUPABASE_ANON_KEY:"sb_publishable_no7fz89mHIciAeQfAeIl7w_zPQ16ads"};
